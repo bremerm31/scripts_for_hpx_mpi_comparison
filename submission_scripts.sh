019c0c5ade583f220d4fbc129939bb3d1acf86b3
Max Bremer <bremerm31@gmail.com>
2017-10-27 17:34:14 -0500

    Added submission functions to `submission_scripts.sh` for submitting jobs on stampede2

--- submission_scripts.sh
+++ submission_scripts.sh
@@ -4,9 +4,9 @@
 #submit_peano_serial generates temporary slurm submission files
 #  and submits them
 #Inputs:
-#  Job Name
-#  Time
-#  Script
+#  (1) Job Name
+#  (2) Time
+#  (3) Script
 function submit_peano_serial {
 
 cat > slurm.sub <<EOL
@@ -32,4 +32,84 @@ echo "${3}" >> slurm.sub
 
 echo "Submitting job..."
 sbatch slurm.sub
+}
+
+#submit_stampede2_serial generates temporary slurm submission files and submits them
+#Inputs:
+#  (1) Job Name
+#  (2) Time (in hours:minutes:seconds format)
+#  (3) Script
+function submit_stampede2_serial {
+
+cat > slurm.sub <<EOL
+#!/bin/bash
+#--------------------------------------------
+# Scripts generated for serial submission of
+# SLURM script on TACC's Stampede2 cluster
+#--------------------------------------------
+
+#SBATCH -p normal
+#SBATCH -N 1
+#SBATCH -n 1
+
+#SBATCH -J ${1}
+#SBATCH -o ${1}.%j
+#SBATCH -t ${2}
+
+#SBATCH -A TG-DMS080016N         # Specify allocation to charge against
+
+module load gcc
+
+EOL
+
+echo "${3}" >> slurm.sub
+
+echo "Submitting job..."
+sbatch slurm.sub
+}
+
+
+#submit_stampede2_parallel generates temporary slurm submission files and submits them
+#Inputs:
+#  (1) Job Name
+#  (2) Time (in hours:minutes:seconds format)
+#  (3) Number of nodes (N)
+#  (4) Number of processes (n)
+#  (5) Script
+function submit_stampede2_parallel {
+
+#need to select the appropriate queue based on required number of nodes
+queue=$([ "${3}" -gt "256" ] && echo "large" || echo "normal")
+
+if [ "${3}" -gt "2048" ]; then
+  echo "Error: Unable to submit jobs with more than 2048 nodes"
+  exit 1
+fi
+
+cat > slurm.sub <<EOL
+#!/bin/bash
+#--------------------------------------------
+# Scripts generated for serial submission of
+# SLURM script on TACC's Stampede2 cluster
+#--------------------------------------------
+
+#SBATCH -p ${queue}
+#SBATCH -N ${3}
+#SBATCH -n ${4}
+
+#SBATCH -J ${1}
+#SBATCH -o ${1}.%j
+#SBATCH -t ${2}
+
+#SBATCH -A TG-DMS080016N         # Specify allocation to charge against
+
+module load gcc
+
+EOL
+
+echo "${5}" >> slurm.sub
+
+echo "Submitting job..."
+cat slurm.sub
+#sbatch slurm.sub
 }
